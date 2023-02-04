66b3bd3e5da67b320609198a75a542046beb1593
최덕경 <hjcdg197@gmail.com>
2023-02-04 22:05:10 +0900

    [Change] 프로젝트명 변경

--- drf-tutorial/asgi.py
+++ drf-tutorial/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for drf_tutorial project.
+ASGI config for drf-tutorial project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_tutorial.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf-tutorial.settings')
 
 application = get_asgi_application()
--- drf-tutorial/urls.py
+++ drf-tutorial/urls.py
@@ -1,4 +1,4 @@
-"""drf_tutorial URL Configuration
+"""drf-tutorial URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
